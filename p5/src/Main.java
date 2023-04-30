27792b57466809111dd2fb58af5ff95f783307d1
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-04-30 14:53:15 -0300

    p5

---
+++ p5/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> pila = new Stack<String>();
+        pila.push("Estoy");
+        pila.push("apilando");
+        pila.push("cositas");
+
+        System.out.println(pila);
+    }
+}
